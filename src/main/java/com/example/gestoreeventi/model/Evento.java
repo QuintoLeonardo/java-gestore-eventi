af256e5cd15325160350c1fabc854a8d7181c6cd
QuintoLeonardo <165834456+QuintoLeonardo@users.noreply.github.com>
2024-08-03 13:11:10 +0100

    added new file Eveneto.java

---
+++ src/main/java/com/example/gestoreeventi/model/Evento.java
@@ -0,0 +1,11 @@
+package com.example.gestoreeventi.model;
+
+import java.time.LocalDate;
+
+public class Evento {
+    private String titolo;
+    private LocalDate data;
+    private int numeroPostiTotali;
+    private int numeroPostiPrenotati;
+
+}
