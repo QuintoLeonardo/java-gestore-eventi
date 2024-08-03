d0b3c1837e4692866867228f942300e09984a0bc
QuintoLeonardo <165834456+QuintoLeonardo@users.noreply.github.com>
2024-08-03 12:58:24 +0100

    Inizio progetto Gestore Eventi con la struttura base e la configurazione Spring Boot

---
+++ src/main/java/com/example/gestoreeventi/GestoreEventiApplication.java
@@ -0,0 +1,7 @@
+package com.example.gestoreeventi;
+
+import com.example.gestoreeventi.model.Evento;
+import java.time.LocalDate;
+import java.util.Scanner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
