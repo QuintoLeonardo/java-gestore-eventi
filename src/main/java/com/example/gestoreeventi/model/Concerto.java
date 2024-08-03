6855ad279623de219cee58c822e903124e5fa923
QuintoLeonardo <165834456+QuintoLeonardo@users.noreply.github.com>
2024-08-03 13:27:52 +0100

    Aggiornamento con getter e setter
    Ora e prezzo

--- src/main/java/com/example/gestoreeventi/model/Concerto.java
+++ src/main/java/com/example/gestoreeventi/model/Concerto.java
@@ -4,3 +4,33 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
+
+public class Concerto extends Evento {
+    private LocalTime ora;
+    private double prezzo;
+
+    public Concerto(String titolo, LocalDate data, int numeroPostiTotali, LocalTime ora, double prezzo) {
+        super(titolo, data, numeroPostiTotali);
+        this.ora = ora;
+        this.prezzo = prezzo;
+    }
+
+    // Getter e setter
+    public LocalTime getOra() {
+        return ora;
+    }
+
+    public void setOra(LocalTime ora) {
+        this.ora = ora;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    
+    
